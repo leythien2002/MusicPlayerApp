be795c785f05b0f1c0dd04b278ec80018a6e0eb9
Thiện Lê <77376115+leythien2002@users.noreply.github.com>
2023-04-12 16:13:51 +0700

    Fix

--- app/src/main/java/com/example/project2_mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/project2_mediaplayer/MainActivity.java
@@ -238,42 +238,6 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    }
 
-    private void startPlaying(Music music, int index) {
-
-        Intent i=new Intent(this,MusicPlaying.class);
-        Bundle bundle=new Bundle();
-        bundle.putInt("index",index);
-        bundle.putSerializable("object_music",music);
-        bundle.putBoolean("isPlaying",isPlaying);
-
-        i.putExtras(bundle);
-        startActivity(i);
-    }
-    private void sendActionToService(int action){
-        Intent i=new Intent(this,MyService.class);
-        Bundle bundle=new Bundle();
-        bundle.putSerializable("object_music",music);
-        bundle.putInt("index",indexMain);
-        bundle.putBoolean("checkPlay",isPlaying);
-//        bundle.putInt("sizeList",listSong.size());
-        i.putExtras(bundle);
-        i.putExtra("action_music_service",action);//cai nay licen quan toi receiver .
-        startService(i);
-    }
-
-    //checkpermission granted ?
-//    private void getPermiss(){
-//        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
-//            return;
-//        }
-//        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
-//            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-//        }
-//        else{
-//            String[] permission={Manifest.permission.POST_NOTIFICATIONS};
-//            requestPermissions(permission,REQUEST_PERMISSION_CODE);
-//        }
-//    }
 
 
 }
